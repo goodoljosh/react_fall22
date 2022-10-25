e9930a69844210e368c233e03a99b2953267123b
goodoljosh <findley_joshua1@columbusstate.edu>
2022-10-25 22:24:44 +0000

    Edited React

---
+++ classdemo/src/App.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
+//jest testing --> unit testing for files
+//when app starts makes sure text learn react is avaliable if not it passes undefined 
