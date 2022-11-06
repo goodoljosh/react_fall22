eacaca40e70b73aeb926512736040dba8b93010b
goodoljosh <findley_joshua1@columbusstate.edu>
2022-11-06 19:29:57 +0000

    Added light and dark themes.

--- classdemo/src/components/Header.jsx
+++ classdemo/src/components/Header.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
+import MainBody from "./MainBody";
 
-class Header extends React.Component {
+class Header extends Component {
     //constructor for state
-    constructor(props){
-      super(props)
-      this.state = {
-        count: 0, 
-        theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light"
-
-       }
+    constructor(props) {
+        super(props)
+        this.state = {
+            count: 0,
+            theme: window.matchMedia('(prefers-color-scheme: dark)').matches ? "dark" : "light",
+            name: ""
+        }
     }
 
     // exp ? true : false; if(exp){true} else {false}  
@@ -16,22 +17,28 @@ class Header extends React.Component {
 
     render() {
         const { linkText, linkUrl } = this.props
+        const name = this.state.name
         //this.props.linkText
         return (
-            <header className={`App-header ${this.state.theme} `}>
-                <p className = {"testing-this" + this.state.theme}>
+            <header className={`App-header ${this.state.theme}`}>
+                <p className={"testing-this" + this.state.theme}>
                     Edit <code>src/App.js</code> and save to reload.
                 </p>
                 <a
                     className="App-link" href={linkUrl} target="_blank" rel="noopener noreferrer">
                     {linkText}
                 </a>
+                <div>
+                    <h3> Your stateful name is: <i>{name}</i></h3>
+                    <input type="text" value={name} onChange={(e) => this.setState({name : e.target.value})} />
+                </div >
                 <span>
                     You have clicked on the button {this.state.count} times!
                 </span>
-                <button onClick={() => this.setState({count: this.state.count + 1})}>
+                <button onClick={() => this.setState({ count: this.state.count + 1 })}>
                     Increment Count
                 </button>
+                <MainBody />
             </header>
         )
 
--- classdemo/src/index.js
+++ classdemo/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './App.scss';
+import './sass/App.scss';
 import App from './App';
 
 
